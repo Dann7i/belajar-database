1750faa38a0f22e7e6c42ba477f33949ff8a9528
Dann7i <hermawanwildan78@gmail.com>
2025-08-17 09:26:25 +0700

    file path

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express"
 import path from "path"
+import { fileUrlToPath } from "url"
+
 const app = express()
 const port = 3000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.listen(port, () => {
   console.log(`berhasil terhubung ke port: ${port}`)
